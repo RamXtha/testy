9c7b313ddc7343bd017492e6180209859eb42b59
Ayush Shrestha <ayushxhrextha@gmail.com>
2023-02-01 19:56:05 +0545

    added file

---
+++ a.py
@@ -0,0 +1,25 @@
+
+o
+password='secret'
+[[LIST]sss:vomit:img src=x onerror=alert(document.domain)//:vomit:<a>dfdf</a>
+
+[PHP]
+
+
+[LIST]
+:
+[/PHP]
+vomit
+vv
+:
+[/LIST]
+aa
+[/LIST]LIST]
+
+sdsdsdsd
+[PHP]phpinfo([*]4)[/PHP]
+
+[PHP]phpinfo([*]4564[LIST=1])[/PHP]
+
+[/LIST]
+[/LIST]
